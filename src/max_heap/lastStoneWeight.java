ecfa06cd7b1471f1f723c52c31b3158a22ad3ec8
nian <1334079243@qq.com>
2020-12-30 20:51:44 +0800

    每日一题

--- src/max_heap/lastStoneWeight.java
+++ src/max_heap/lastStoneWeight.java
@@ -1,10 +1,23 @@
 package max_heap;
+/**
+ *
+ * title 1046.最后一块石头的重量
+ * 有一堆石头，每块石头的重量都是正整数。
+ *
+ * 每一回合，从中选出两块 最重的 石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+ *
+ * 如果 x == y，那么两块石头都会被完全粉碎；
+ * 如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。
+ * 最后，最多只会剩下一块石头。返回此石头的重量。如果没有石头剩下，就返回 0。
+ *
+ */
 
 import java.util.PriorityQueue;
-
+//最大堆
 public class lastStoneWeight {
     public int lastStoneWeight1(int[] stones) {
         int n = stones.length;
+        //优先队列
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> b - a);
         for (int stone : stones) {
             pq.offer(stone);
