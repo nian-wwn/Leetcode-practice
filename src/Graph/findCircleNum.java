b339df75795de578c05490262acaaac5709a88b7
nian <1334079243@qq.com>
2021-01-07 20:51:34 +0800

    每日一题

---
+++ src/Graph/findCircleNum.java
@@ -0,0 +1,25 @@
+package Graph;
+
+public class findCircleNum {
+    public int findCircleNum1(int[][] isConnected) {
+        int provinces = isConnected.length;
+        boolean[] visited = new boolean[provinces];
+        int circles = 0;
+        for (int i = 0; i < provinces; i++) {
+            if (!visited[i]) {
+                dfs(isConnected, visited, provinces, i);
+                circles++;
+            }
+        }
+        return circles;
+    }
+
+    public void dfs(int[][] isConnected, boolean[] visited, int provinces, int i) {
+        for (int j = 0; j < provinces; j++) {
+            if (isConnected[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                dfs(isConnected, visited, provinces, j);
+            }
+        }
+    }
+}
