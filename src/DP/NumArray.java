c81feda39685856e03a439a43837447851ec5506
nian <1334079243@qq.com>
2021-03-01 21:43:27 +0800

    每日一题

---
+++ src/DP/NumArray.java
@@ -0,0 +1,17 @@
+package DP;
+
+public class NumArray {
+    int[] sum;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        sum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+}
